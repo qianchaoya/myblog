d48cb482375d817a86bb586eea4517f9747b5531
qian <qcy@qq.com>
2017-01-17 20:31:14 +0800

    Site updated: 2017-01-17 20:31:14

--- js/main.js
+++ js/main.js
@@ -6,23 +6,11 @@ $(document).ready(function() {
     $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
     $('.content-wrapper').addClass('animated slideInLeft');
   });
-
-  $('a.about-button').click(function() {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    currentWidth = $('.panel-cover').width();
-    $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
-    $('.content-wrapper').addClass('animated slideInLeft');
-  });
-
-
+ 
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
- if (window.location.hash && window.location.hash == "#about") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
-
 
   if (window.location.pathname != "/") {       // if hexo in subdir of site, should change this line
     $('.panel-cover').addClass('panel-cover--collapsed');
@@ -37,8 +25,5 @@ $(document).ready(function() {
     // $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
- $('.navigation-wrapper .about-button').click(function() {
-    // $('.navigation-wrapper').toggleClass('visible');
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
-  });
+
 });
