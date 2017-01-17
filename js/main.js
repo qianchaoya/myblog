045240d21db4c08061ef31815319b15897f9b283
qian <qcy@qq.com>
2017-01-17 20:28:33 +0800

    Site updated: 2017-01-17 20:28:32

--- js/main.js
+++ js/main.js
@@ -7,9 +7,22 @@ $(document).ready(function() {
     $('.content-wrapper').addClass('animated slideInLeft');
   });
 
+  $('a.about-button').click(function() {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    currentWidth = $('.panel-cover').width();
+    $('.panel-cover').addClass('animated panel-cover--collapsed slideInLeft');
+    $('.content-wrapper').addClass('animated slideInLeft');
+  });
+
+
+
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
+ if (window.location.hash && window.location.hash == "#about") {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+
 
   if (window.location.pathname != "/") {       // if hexo in subdir of site, should change this line
     $('.panel-cover').addClass('panel-cover--collapsed');
@@ -24,5 +37,8 @@ $(document).ready(function() {
     // $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
-
+ $('.navigation-wrapper .about-button').click(function() {
+    // $('.navigation-wrapper').toggleClass('visible');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
+  });
 });
